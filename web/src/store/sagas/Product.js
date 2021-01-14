6cd80c4daff143419533dc5bae8747d25652a9c6
thiagomedina <thiagomedina001@gmail.com>
2021-01-14 02:54:54 -0300

    created the product and listed

--- web/src/store/sagas/Product.js
+++ web/src/store/sagas/Product.js
@@ -6,18 +6,28 @@ import { Types } from '../ducks/Product';
 function* addProduct(payload) {
     let response = {};
     try {
-
-        console.log(payload)
+        response = yield call(api.post, 'product', payload.payload);
 
     } catch (e) {
         console.log(e);
-        yield put({ type: Types.GET_TOP_TRACKS_SUCCESS, errorMessage: 'error' });
+        yield put({ type: Types.ADD_PRODUCT_FAILURE, errorMessage: 'error' });
     }
 }
 
+function* getProduct() {
+    let response = {};
+    try {
+        response = yield call(api.get, 'product');
+        yield put({ type: Types.ADD_PRODUCT_SUCCESS, payload: response.data });
+    } catch (e) {
+        console.log(e);
+        yield put({ type: Types.GET_PRODUCT_FAILURE, errorMessage: 'error' });
+    }
+}
 
 export default function* () {
     yield all([
         takeLatest(Types.ADD_PRODUCT, addProduct),
-    ]); 
+        takeLatest(Types.GET_PRODUCT, getProduct),
+    ]);
 }
--- web/src/view/ProductList/index.jsx
+++ web/src/view/ProductList/index.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Header from '../../components/Header'
 import ProductCard from '../../components/ProductCard'
+import { Creators as ProductActions } from '../../store/ducks/Product';
+
 
 import { Content } from './styles'
 const ProductList = () => {
+    const dispatch = useDispatch()
+    const products = useSelector(state => state.Product?.data)
+
+    useEffect(() => {
+        dispatch(ProductActions.getProduct())
+    }, [dispatch])
+
+
     return (
         <>
             <Header />
             <Content>
                 <h1>Produtos Cadastrados</h1>
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
+                {products && products.map(item => <ProductCard prod={item} />)}
             </Content>
         </>
     )
